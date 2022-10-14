0a387a9b29328f1063d59f3cce9c16bc4d9b3703
Etienne219 <114392023+Etienne219@users.noreply.github.com>
2022-10-14 15:36:32 +0200

    scripts update

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const buttons = document.querySelectorAll('button');
-const arrayOfButtons = Array.from(buttons);
-const testBtn = document.getElementById("allClear");
+const arrayOfButtons = Array.from(document.querySelectorAll('button'));
 const display = document.querySelector(".screenOutput");
 
 arrayOfButtons.forEach(btn => {
-    btn.addEventListener('click', ()=>{
-        display.textContent = btn.textContent;
+    btn.addEventListener('click', (e)=>{
+        switch (e.target.innerText) {
+            case "AC":
+                display.innerText = "";
+            break;
+            case "=":
+                try {
+                    display.innerText = eval(display.innerText);
+                  } catch {
+                    display.innerText = "Error";
+                  }
+            break;
+            default:
+                display.innerText += e.target.innerText;
+            break;
+        }
     })
 });
+
